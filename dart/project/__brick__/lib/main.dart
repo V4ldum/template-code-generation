dce34e0dd0be64a8751f9e13b8e69a13b8e37c44
V4ldum <56064897+V4ldum@users.noreply.github.com>
2024-03-09 17:36:56 +0100

    fix(project): added supportedlocales to MaterialApp.router to enable switching language based on the device's settings

--- dart/project/__brick__/lib/main.dart
+++ dart/project/__brick__/lib/main.dart
@@ -24,6 +24,7 @@ class MyApp extends StatelessWidget {
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
       ],
+      supportedLocales: S.delegate.supportedLocales,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
